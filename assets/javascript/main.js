f15306110c1c7e2c5a40288cc671670e5e70dcac
Thomas Winters <winterst85@gmail.com>
2018-08-06 21:40:46 -0500

    created files

---
+++ assets/javascript/main.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+
+// Variables:
+const celebs = [
+    'emma stone',
+    'emma watson',
+    'chris hemsworth',
+    'steve carell',
+    'liam neeson',
+    'jim carrey'  
+]
+
+//create buttons for the array items
+//display on page
+//add form for user input
+//take in value and create a button for that input
+//append to the original buttons
+        //does not clear the gifs
+//add user input to array
+
+//User clicks on a button, ajax giphy api
+//take in gif value
+//put 10 on the page
+//dipslay the picture, rating
+//make the gif static until clicked, then static once clicked again
+//once user selects another button, empty the page.
+
+
+
+
+});
